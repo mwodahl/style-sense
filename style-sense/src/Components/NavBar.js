6d207532f35ea294db3ae47140d91bd89b6c271e
unknown <jgoodr10@uwyo.edu>
2023-04-13 01:15:27 -0600

    Weather with location

--- style-sense/src/Components/NavBar.js
+++ style-sense/src/Components/NavBar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Card, Flex, Menu, MenuItem, MenuButton } from '@aws-amplify/ui-react'
-import '../css/Shared.css'
+import '../css/Shared.css';
+import '../css/NavBar.css';
 
 function NavBar(props) {
 
@@ -23,13 +24,29 @@ function NavBar(props) {
     borderRadius: '5px'
   }
 
+  const [data, setData] = useState({});
+  const [lat, setLat] = useState({});
+  const [lon, setLon] = useState({});
+  navigator.geolocation.getCurrentPosition(function (position) {
+    setLat(position.coords.latitude.toString());
+    setLon(position.coords.longitude.toString());
+  });
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=e2ab1469ff86523278c10e93f6baf4b3&units=imperial";
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [url]);
+
+
   return (
     <View
       position="fixed"
       height="10%"
       top="0"
       width="100%"
-      style = {viewStyle}
+      style={viewStyle}
     >
       <Flex
         className='background'
@@ -39,62 +56,64 @@ function NavBar(props) {
         gap='0rem'
       >
         <Card
-            height="100%"
-            width="calc(100% / 3)"
-            className='background'
+          height="100%"
+          width="calc(100% / 3)"
+          className='background'
+        >
+          <Flex
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
           >
-            <Flex
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="center"
-            >
-              <h3
+            <h3
               className='navHeader'>
-                StyleSense Logo
-                </h3>
-            </Flex>
-          </Card>
-          <Card
-            height="100%"
-            width="calc(100% / 3)"
-            className='background'
+              StyleSense Logo
+            </h3>
+          </Flex>
+        </Card>
+        <Card
+          height="100%"
+          width="calc(100% / 3)"
+          className='background'
+        >
+          <Flex
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
           >
-            <Flex
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <h3
-              className='navHeader'
-              >Weather Info?</h3>
-            </Flex>
-          </Card>
-          <Card
-            height="100%"
-            width="calc(100% / 3)"
-            className='background'
+            <View className="weather">
+              {data.name ? <h2>{data.name}</h2> : null}
+              {data.main ? <h3>{data.main.temp.toFixed()}°F</h3> : null}
+              {data.weather ? <h3>{data.weather[0].main}</h3> : null}
+            </View>
+          </Flex>
+        </Card>
+        <Card
+          height="100%"
+          width="calc(100% / 3)"
+          className='background'
+        >
+          <Flex
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
           >
-            <Flex
-              direction="row"
-              justifyContent="flex-end"
-              alignItems="center"
-            >
-              <Menu
+            <Menu
               menuAlign='end'
               style={menuStyle}
               trigger={
                 <MenuButton
-                style={profileStyle}>
+                  style={profileStyle}>
                   {(props.User[0]).toUpperCase()}
                 </MenuButton>
               }>
-                <MenuItem className='profileOption' onClick={props.SignOut}>Sign Out</MenuItem>
-                <MenuItem className='profileOption' disabled={true} onClick={() => console.log("Profile")}>Profile</MenuItem>
-              </Menu>
-            </Flex>
-          </Card>
-      </Flex>
-    </View>
+              <MenuItem className='profileOption' onClick={props.SignOut}>Sign Out</MenuItem>
+              <MenuItem className='profileOption' disabled={true} onClick={() => console.log("Profile")}>Profile</MenuItem>
+            </Menu>
+          </Flex>
+        </Card>
+      </Flex >
+    </View >
   );
 
 }
